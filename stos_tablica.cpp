316b604c492d9a5eff6e655184e5a27e55eb1d23
Artur <235239@student.pwr.edu.pl>
2018-03-11 17:55:01 +0100

    utworzone funkcje sprawdzajace wielkosc i pustość oraz istnieje menu, analogicznie jak w kolejce

--- stos_tablica.cpp
+++ stos_tablica.cpp
@@ -1,10 +1,66 @@
 #include <iostream>
 #include <windows.h>
 
+
+using namespace std;
+
 int dane [6];
 int rozmiar;
 
+
+void wielkosc()
+{
+    cout<<endl<<"Liczba elementow na stosie: "<<rozmiar;
+    Sleep(1000);
+}
+
+void czy_pusty()
+{
+
+    if (rozmiar==0) cout<<endl<<"stos pusty";
+    else cout<<endl<<"wystempuja elementy";
+    Sleep(1000);
+}
+
 int main ()
 {
+    int wybor;
+
+
+    do{
+        //wyswietl_stos();
+
+
+        cout << "MENU GLOWNE STOSU:"<<endl;
+        cout << "******************"<<endl;
+        cout << "1. Dodaj element "<<endl;
+        cout << "2. usun element "<<endl;
+       cout << "3. rozmiar stosu "<<endl;
+        cout << "4. czy stos jest pusty? "<<endl;
+        cout << "5. Koniec programu "<<endl;
+        cout << "wybor opcji: ";
+        cin >> wybor;
+
+
+
+        switch (wybor)
+        {
+        case 1:
+          //  dodaj();
+            break;
+
+        case 2:
+           // usun();
+            break;
+              case 3:
+            wielkosc();
+            break;
+
+        case 4:
+            czy_pusty();
+            break;
+            }
+}
+            while (wybor!=5);
     return 0;
 }
